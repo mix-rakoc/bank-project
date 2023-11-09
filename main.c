acda52ade39d2d30f4fa092908373e791a55640d
Mihailo Rakocevic <culobula@gmail.com>
2023-11-09 11:46:53 +0100

    Prvi i drugi commit

---
+++ main.c
@@ -0,0 +1,18 @@
+	#include <stdio.h>
+
+int main() {
+
+
+	printf("Dobar dan mi smo banka koju opciju zelite
+			1. Kredit 
+			2. Minus");
+	
+	int a = -1;
+	scanf();
+
+	while(a!= 1 && a!= 2)
+		printf("Izabrali ste pogresnu opciju mozete samo 1. ili 2.");
+	
+
+	return 0;
+}
