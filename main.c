7c2c841f98407b10536d7b1e0b90878b1a0ffc8a
Mihailo Rakocevic <culobula@gmail.com>
2023-11-09 18:07:10 +0100

    Third phase

--- main.c
+++ main.c
@@ -18,7 +18,49 @@ int main () {
 			printf("Greska mozete samo 1 ili 2");		
 		}
 	}
-	
+	if(opcija == 1)
+	{
+		printf("KREDIT\n");
+		printf("Ako se slazete sa nasom kamatnom stopom pritisnite taster broj 1 ako ne broj 2\n");
+		int opcijaKredit = -1;
+		while(opcijaKredit != 1 && opcijaKredit != 2)
+		{
+			scanf("%d" , &opcijaKredit);
+			if(opcijaKredit != 1 && opcijaKredit != 2)
+			{
+				printf("Greska mozete samo 1 ili 2\n");
+			}
+				
+		}
+		if(opcijaKredit == 1)
+		{
+			printf("Unesite vasa primanja:\n");
+			float primanja = -2;
+			while(primanja <= 0)
+			{	
+			
+				scanf("%f" , &primanja);
+				if (primanja <= 0)
+				{
+					printf("Greska unesite primanja koja su veca od 0\n");
+				}
+			}
+		}
+	}else
+	{
+		printf("MINUS\n");
+		printf("Unesite vasa primanja!:\n");
+		float primanja = -1;
+		while(primanja <= 0)
+		{
+			scanf("%f", &primanja);
+			if(primanja <= 0)
+			{
+				printf("Greska unesite primanja koja su veca od nule\n");
+			}
+		}
+		
+	}
 
 
 
