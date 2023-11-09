fb0d8fb9d6d835e2db6b0c2c6c32b37a12c730ba
Mihailo Rakocevic <culobula@gmail.com>
2023-11-09 16:07:37 +0100

    Ispravka drugi commit

--- main.c
+++ main.c
@@ -8,7 +8,17 @@ int main() {
 			1. Kredit 
 			2. Minus");
 
+	scaft(a);
+	int a = -1;
+
+	while(a != 1 && a != 2){
+		printf("Uneli ste nepostojecu opciju");
+	}
+
 	return 0;
 }
+
+
+
 =======
 >>>>>>> parent of acda52a (Prvi i drugi commit)
